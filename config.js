e82ec3c0c381f2bdc971c083a895dc6491b97dfd
walxlgf <guanghui-li@163.com>
2018-03-19 16:53:22 +0800

    '修改变量'

--- config.js
+++ config.js
@@ -3,14 +3,14 @@ module.exports = {
 
     // 小程序 appId 和 appSecret 
     // 请到 https://mp.weixin.qq.com 获取 AppID 和 AppSecret
-    appId:process.env.APP_ID || 'wxc14d0ff891dbbb64', 
-    appSecret:process.env.APP_SECRET || '654f6c6559336fa79d13c85e4cb2e080', 
+    appId:'wxc14d0ff891dbbb64', 
+    appSecret:'654f6c6559336fa79d13c85e4cb2e080', 
 
     // mongodb 连接配置，生产环境请使用更复杂的用户名密码
     // mongoHost: '127.0.0.1', 
-    mongoHost: process.env.MONGO_HOST || 'mongodb', 
-    mongoPort: process.env.MONGO_PORT || '27017', 
-    mongoUser: process.env.MONGO_USER || 'weapp', 
-    mongoPass: process.env.MONGO_PASS || 'weapp-dev', 
-    mongoDb: process.env.MONGO_DB || 'weapp'
+    mongoHost: 'mongodb', 
+    mongoPort: '27017', 
+    mongoUser: 'weapp', 
+    mongoPass: 'weapp-dev', 
+    mongoDb: 'weapp'
 };
