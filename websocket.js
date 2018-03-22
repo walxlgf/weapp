4bba34709215c92df5bd5ebed96de9eeb2a08e0d
walxlgf <guanghui-li@163.com>
2018-03-22 19:59:49 +0800

    更新websocket

--- websocket.js
+++ websocket.js
@@ -1,5 +1,9 @@
+// 引入 url 模块用于解析 URL
+const url = require('url');
 // 引入 ws 支持 WebSocket 的实现
 const ws = require('ws');
+// 引入人类玩家
+const HumanPlayer = require('./game/HumanPlayer');
 
 // 导出处理方法
 exports.listen = listen;
@@ -11,10 +15,18 @@ exports.listen = listen;
  */
 function listen(server, sessionMiddleware) {
     // 使用 HTTP Server 创建 WebSocket 服务，使用 path 参数指定需要升级为 WebSocket 的路径
-    const wss = new ws.Server({ server, path: '/ws' });
+    const wss = new ws.Server({ server });
+
+    // 同时支持 /ws 和 /game 的 WebSocket 连接请求 
+    wss.shouldHandle = (request) => { 
+        const path = url.parse(request.url).pathname; 
+        request.path = path; 
+        return ['/ws', '/game'].indexOf(path) > -1; 
+    }; 
 
     // 监听 WebSocket 连接建立
-    wss.on('connection', (ws,request) => {// 要升级到 WebSocket 协议的 HTTP 连接
+    wss.on('connection', (ws, request) => {
+        // request: 要升级到 WebSocket 协议的 HTTP 连接
 
         // 被升级到 WebSocket 的请求不会被 express 处理，
         // 需要使用会话中间节获取会话
@@ -26,9 +38,14 @@ function listen(server, sessionMiddleware) {
                 ws.close();
                 return;
             }
-            // 保留这个日志的输出可让实验室能检查到当前步骤是否完成
             console.log(`WebSocket client connected with openId=${session.userInfo.openId}`);
-            serveMessage(ws, session.userInfo);
+
+            // 根据请求的地址进行不同处理 
+            switch (request.path) { 
+                case '/ws': return serveMessage(ws, session.userInfo); 
+                case '/game': return serveGame(ws, session.userInfo); 
+                default: return ws.close();
+            }
         });
     });
 
@@ -55,4 +72,19 @@ function serveMessage(ws, userInfo) {
 
     // 连接后马上发送 hello 消息给会话对应的用户
     ws.send(`Server: 恭喜，${userInfo.nickName}`);
+}
+
+/**
+ * 使用 WebSocket 进行游戏服务
+ */
+function serveGame(ws, userInfo) {
+    const user = { 
+        uid: userInfo.openId, 
+        uname: userInfo.nickName, 
+        uavatar: userInfo.avatarUrl 
+    }; 
+    // 创建玩家 
+    const player = new HumanPlayer(user, ws); 
+    // 玩家上线
+    player.online();
 }
